bdccbb5fa548f31ea26a7eb6d75d58be21d13eeb
Klose45 <akongseh45@gmail.com>
2026-02-20 11:34:49 +0100

    CineVerse is a mordern movie web appliction built with React Js that allows user to discover and explor the latest movies in real time.

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "91c389a9";
+const BASE_URL = "https://www.omdbapi.com/";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
